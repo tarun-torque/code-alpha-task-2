f1e454e61caa26ff536f61912cbaa1b98363373d
TARUN TEJASHWI SHARMA <torque061@gmail.com>
2023-07-27 15:11:19 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,76 @@
+function nightView(){
+
+  
+
+  document.getElementById('head-test').style.backgroundColor='#062C30'
+  document.getElementById('web-name').style.color='white'
+
+
+  document.getElementById('s1').style.color='white'
+  document.getElementById('s2').style.color='white'
+  document.getElementById('s3').style.color='white'
+
+
+ document.getElementById('test').style.backgroundColor='#062C30'
+
+ document.getElementById('nb').style.backgroundColor='#062C30'
+ document.getElementById('nb-head').style.color='#F9F6EE'
+
+ document.getElementById('rec-1').style.backgroundColor='#062C30'
+ document.getElementById('rec-2').style.backgroundColor='#062C30'
+
+ document.getElementById('eb').style.backgroundColor='#062C30'
+ document.getElementById('eb-head').style.color='#F9F6EE'
+
+
+ document.getElementById('sb').style.backgroundColor='#062C30'
+ document.getElementById('sb-head').style.color='#F9F6EE'
+
+ document.getElementById('big-rec').style.backgroundColor='#062C30'
+
+
+ document.getElementById('night-mode').style.display='none'
+document.getElementById('day-mode').style.display='block'
+
+}
+
+
+ function dayView(){
+
+  document.getElementById('day-mode').style.backgroundColor='black'
+  document.getElementById('day-mode').style.color='white'
+
+  
+  document.getElementById('s1').style.color='black'
+  document.getElementById('s2').style.color='black'
+  document.getElementById('s3').style.color='black'
+
+
+
+
+   document.getElementById('test').style.backgroundColor='#FFF2CE'
+  
+   document.getElementById('head-test').style.backgroundColor='#FEFEFE'
+   document.getElementById('web-name').style.color='black'
+
+   
+  
+   document.getElementById('nb').style.backgroundColor='#FFF2CE'
+   document.getElementById('nb-head').style.color='black'
+  
+   document.getElementById('rec-1').style.backgroundColor='#FFFEFB'
+   document.getElementById('rec-2').style.backgroundColor='#FFFEFB'
+  
+   document.getElementById('eb').style.backgroundColor='#FFF2CE'
+   document.getElementById('eb-head').style.color='black'
+  
+  
+   document.getElementById('sb').style.backgroundColor='#FFF2CE'
+   document.getElementById('sb-head').style.color='black'
+  
+   document.getElementById('big-rec').style.backgroundColor='#FFFEFB'
+  
+   document.getElementById('day-mode').style.display='none'
+   document.getElementById('night-mode').style.display='block'
+ 
+ }
